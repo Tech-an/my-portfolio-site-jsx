425ef46e94b178d51dcbe15e8f536cee0d3d0709
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2023-02-16 00:55:13 +0900

    layoutの作成

---
+++ components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 style={{ height: "100vh", width: "20vw", backgroundColor: "green" }}>
+        ヘッダーコンテンツ部
+      </h1>
+    </div>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ height: "100vh", width: "80vw", backgroundColor: "blue" }}>
+        メインコンテンツ部
+      </h1>
+    </div>
+  );
+}
