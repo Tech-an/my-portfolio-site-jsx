28ad5ce53a491813865ef0b40edcfb04c2070d23
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2023-02-16 07:04:08 +0900

    scroll-barの実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,48 @@
 import Home from "./home";
+import Layout from "components/layout";
 import About from "./about";
 import Career from "./career";
 
+import { useState, useEffect } from "react";
+
+import styles from "../styles/index.module.css";
+
 export default function Index() {
+  const [height, setHeight] = useState(0);
+  useEffect(() => {
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    const handleScroll = () => {
+      setHeight((window.scrollY / totalHeight) * 100);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
       <Home />
-      <About />
-      <Career />
-      <h1
-        style={{
-          height: "200vh",
-          width: "80vw",
-          backgroundColor: "blue",
-        }}
-      >
-        メインコンテンツ部
-      </h1>
+      <div
+        className={styles.progressbar}
+        style={{ height: `${height}%` }}
+      ></div>
+      <div className={styles.scrollPath}></div>
+      <Layout>
+        <section>
+          <About />
+          <Career />
+          <h1
+            style={{
+              height: "200vh",
+              width: "80vw",
+              backgroundColor: "blue",
+            }}
+          >
+            メインコンテンツ部
+          </h1>
+        </section>
+      </Layout>
     </div>
   );
 }
