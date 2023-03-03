bbab6ee15d7f2f18fed1bd49b8ffad4762d78610
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2023-03-03 17:51:58 +0900

    割と無理やりだけどサイドメニューの開閉を実装

--- components/layout.jsx
+++ components/layout.jsx
@@ -2,14 +2,50 @@ import SideNav from "./sidenav/sidenav";
 import Footer from "./footer";
 import styles from "../src/styles/layout.module.css";
 
+import Home from "@/pages/home";
+
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
 export default function Layout({ children }) {
+  const [hidden, setHidden] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 768);
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const sideNavHandler = () => {
+    return (
+      <div className={styles.side_nav_handler}>
+        <FontAwesomeIcon
+          icon={hidden ? faBars : faXmark}
+          onClick={() => setHidden(!hidden)}
+        />
+      </div>
+    );
+  };
+
   return (
-    <div className={styles.container}>
-      <SideNav />
-      <main style={{ width: "100%" }}>
-        {children}
-        <Footer />
-      </main>
+    <div>
+      <Home />
+      <div className={styles.container}>
+        <div className={isMobile && hidden ? styles.hidden : null}>
+          <SideNav />
+        </div>
+        <main className={styles.main_container}>
+          {isMobile ? sideNavHandler() : null}
+          {children}
+          <Footer />
+        </main>
+      </div>
     </div>
   );
 }
