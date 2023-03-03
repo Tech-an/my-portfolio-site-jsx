4569cffb39626ebfb75576ce3e1d5d16e664a4e0
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2023-03-04 05:31:48 +0900

    アコーディオンを実装しました

--- components/accordion/accordion.jsx
+++ components/accordion/accordion.jsx
@@ -1,9 +1,24 @@
 import styles from "./accordion.module.css";
+import { useState } from "react";
 
-export default function Accordion() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlassPlus,
+  faMagnifyingGlassMinus,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function Accordion({ children, text }) {
+  const [open, setOpen] = useState(false);
   return (
-    <div className={styles.container}>
-      <h1>アコーディオンを作成するよ</h1>
+    <div className={styles.accordion} onClick={() => setOpen(!open)}>
+      <div className={styles.contentBx}>
+        <div className={open ? styles.active : null}>
+          <div className={styles.label}>{children}</div>
+          <div className={styles.content}>
+            <p>{text}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
