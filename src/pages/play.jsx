833406c4c03ea2516a1b7e5af2ebf96bb85b15de
Tech-an <sato.tetsuya.c2@s.mail.nagoya-u.ac.jp>
2023-03-05 22:17:58 +0900

    ゲームの追加

--- src/pages/play.jsx
+++ src/pages/play.jsx
@@ -1,18 +1,79 @@
 import styles from "../styles/play.module.css";
 import Slot from "components/play/slot/slot";
 
-import ComingSoon from "components/coming-soon";
+import { useState } from "react";
+import Image from "next/image";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+import slot from "../../public/slot.png";
 
 export default function Play() {
+  const [gameName, setGameName] = useState("slot");
+  const [openMenu, setOpenMenu] = useState(false);
+  const gameContainer = () => {
+    switch (gameName) {
+      case "slot":
+        return <Slot />;
+      default:
+        return null;
+    }
+  };
+  const menuContainer = (gameTitle, imgPath) => {
+    return (
+      <div
+        onClick={() => {
+          setGameName(gameTitle);
+          setOpenMenu(!openMenu);
+        }}
+        className={gameName == gameTitle ? styles.selected : null}
+      >
+        <Image src={imgPath} alt="" />
+        <h3>{gameTitle}</h3>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.container} id="Play">
       <h1 className={`${styles.title} ${styles.sticky}`}>
         <span>P</span>lay
       </h1>
-      <ComingSoon clr="#7700e2" />
-      {/* <div className={styles.game_container}>
-        <Slot />
-      </div> */}
+      <div className={styles.game_container}>{gameContainer()}</div>
+      <div className={styles.game_menus}>
+        <button
+          onClick={() => {
+            setOpenMenu(!openMenu);
+          }}
+        >
+          ゲーム一覧
+        </button>
+        {openMenu ? (
+          <div className={styles.game_menus_container}>
+            <div className={styles.game_menus_content}>
+              <div
+                onClick={() => {
+                  setOpenMenu(!openMenu);
+                }}
+                className={styles.close_button}
+              >
+                <FontAwesomeIcon icon={faCircleXmark} />
+              </div>
+              <h2 className={styles.menu_title}>ゲーム一覧</h2>
+              <nav>
+                {menuContainer("slot", slot)}
+                {menuContainer("slot", slot)}
+                {/* {menuContainer("slot", slot)}
+                {menuContainer("slot", slot)}
+                {menuContainer("slot", slot)}
+                {menuContainer("slot", slot)}
+                {menuContainer("slot", slot)} */}
+              </nav>
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
